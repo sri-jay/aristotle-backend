06c046f1bcb35d51e60afd130e4f9f30506a7e70
Sriduth Jayhari <sriduth.jayhari@gmail.com>
2014-11-12 09:57:47 +0530

    added exception

---
+++ main.py
@@ -0,0 +1,119 @@
+from flask import Flask, render_template, request, jsonify, make_response
+
+# DB imports
+import psycopg2
+
+# Required for generating secret keys
+import random
+import hashlib
+import json
+import urlparse
+
+# for urlparse
+import os
+
+# Setup url parse to read DB login data as environment string
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+app = Flask(__name__, static_url_path = "")
+
+def connect_to_db():
+
+	print "Creating connection object."
+	db_connection = psycopg2.connect(
+		database=url.path[1:],
+		user=url.username,
+		password=url.password,
+		host=url.hostname,
+		port=url.port
+	)
+
+	return db_connection
+
+@app.route("/registerDevice",methods=['GET','POST'])
+def register_device():
+	device_random_code = request.form['random_hash']
+
+	response = None
+
+	STATUS = "REGISTRATION_SUCCEEDED"
+	try:
+		# Creating a random number
+		app_secret = hashlib.sha224(str(random.random())).hexdigest()
+
+		# Get databsae connection
+		connection = connect_to_db()
+
+		# Create cursor
+		cursor = connection.cursor()
+
+		# Build query
+		statement = """INSERT INTO api_secret_keys VALUES(\'%s\')"""%(app_secret)
+
+		# Execute query
+		cursor.execute(statement)
+
+		response = {'STATUS' : STATUS, 'API_SECRET' : app_secret}
+
+		connection.close()
+		
+	except Exception as e:
+		print "DB connection falied!"
+		print e
+		
+		STATUS = "REGISTRATION_FALIED"
+		response = {'STATUS' : STATUS}
+
+	return jsonify(response)
+
+@app.route("/getSession", methods=['GET','POST'])
+def get_session():
+	
+	# Get app secret from device
+	app_secret = request.form['secret_key']
+
+	response = None
+
+	try:
+		# Get connection object
+		connecton = connect_to_db()
+
+		# Ge ta cursor
+		cursor = connection.cursor()
+
+		query = """SELECT COUNT(*) FROM api_secret_keys WHERE key = \'%s\'"""%(app_secret)
+
+		cursor.excute(query)
+
+		data = cursor.fetchall()
+
+		if data[0] == 1:
+
+			STATUS = "SESSION_SUCCESS"
+			SESSION_KEY = hashlib.md5(str(random.random())).hexdigest()
+
+			response = {'STATUS' : STATUS, 'SESSION_KEY' : SESSION_KEY}
+
+		else:
+			STATUS = "SESSION_FAILED"
+			response = {'STATUS' : STATUS}
+
+	except Exception as e:
+
+		print "DB Insert failed."
+		STATUS = "FAILED_TO_AUTH"
+		response = {'STATUS' : STATUS}
+	return jsonify(response)
+
+
+
+@app.route('/')
+def hello:
+	return "Hello!"
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
+	
+
